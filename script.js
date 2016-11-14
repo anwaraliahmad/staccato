dc1a6d59676a39849fcf6f955d81ed19e162b1fd
Anwar Ali-Ahmad <anwar.aliahmad@icloud.com>
2016-11-14 15:16:19 -0800

    Major refactoring

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ var abyssGeo, abyssUniforms, abyssMaterial, abyss;
 
 var heartMaterial, heartUniforms, heartGeometry, heart;
 
-
 function Staccato(scene, camera) {
 
   this.scene = scene;
@@ -21,17 +20,21 @@ function Staccato(scene, camera) {
         type: "f",
         value: 0.0
       },
-      t: {
+      time: {
         type: "f",
         value: 0.0
       },
-      fft: {
-        type: "iv1",
+      frequency: {
+        type: "fv1",
         value: [1.0, 1.0, 1.0]
       },
       mDb: {
         type: "f",
         value: -100.0
+      }, 
+      od: {
+        type: "f",
+        value: 0.0
       }
   };
   // Size for AnalysisNode's fft samples 
@@ -42,10 +45,14 @@ function Staccato(scene, camera) {
   }  
   this.ctx = new (window.AudioContext || window.webkitAudioContext)();
   var ctx = this.ctx;
+  this.analyser = this.ctx.createAnalyser();
+  this.analyser.fftSize = 1024;
+  var analyser = this.analyser;
   this.source;
+  this.waveData = new Float32Array(this.FFT/2);
+  this.mouse = {x: 0, y:0};
+  var objectMat = [];
 
-  //this.fileHandler.bind(this);
-  // Load a new song
   this.load = function (url) {
     // Make a get request to song's URL 
     var r = new XMLHttpRequest();
@@ -60,35 +67,46 @@ function Staccato(scene, camera) {
         buffer = buff;
         scope.play(buffer);
 
-      });
-    }
+      }.bind(this));
+    }.bind(this);
     r.send();
   }
 
 
+  // Play the sound buffer
   this.play = function(buffer) {
     var s = this.ctx.createBufferSource();
     s.buffer = buffer;
     s.connect(this.ctx.destination);
     s.start(0);
     this.source = s;
+    console.log(s);
+  }
+
+  // Calculating average of FFT data
+  this.avg = function (arr) {
+    var s = 0; 
+    for (var i = 0; i < arr.length; i++) {
+      s += arr[i];
+    }
+    return s/arr.length;
   }
 
 
   this.fileDragHover = function (e) {
     e.stopPropagation();
     e.preventDefault();
-    //console.log("fileDragHover triggered");
-    //e.target.className = (e.type === 'dragover' ? 'hover' : '');
+    e.target.className = (e.type === 'dragover' ? 'hover' : '');
   }
-  function initAudio(data) {
-   // if (this.source) source.stop(0);
+
+  this.initAudio = function(data) {
     var source = ctx.createBufferSource();
     if (ctx.decodeAudioData) {
       ctx.decodeAudioData(data, function(buffer) {
         var s = ctx.createBufferSource();
         s.buffer = buffer;
         s.connect(ctx.destination);
+        s.connect(analyser);
         s.start(0);
       }, function (e) {
         console.error(e);
@@ -97,28 +115,97 @@ function Staccato(scene, camera) {
       console.log("Error: could not initialize audio data.");
     }
   }
+
   this.fileHandler = function (e) {
     e.stopPropagation();
     e.preventDefault();
+    e.target.className = "";
     var files = e.target.files || e.dataTransfer.files;
     var reader = new FileReader();
     reader.onload = function (f) {
       var dat = f.target.result;
-      initAudio(dat);
-    }
+      this.initAudio(dat);
+    }.bind(this);
 
     reader.readAsArrayBuffer(files[0]);
   }
 
+  // Setting the scope of event handler functions to be Staccato
+  this.fileHandler = this.fileHandler.bind(this);
+  this.fileDragHover= this.fileDragHover.bind(this);
+  this.initAudio = this.initAudio.bind(this);
 
 
-
-  this.fileDrop = document.getElementById("fileDrop");
+  this.fileDrop = document.getElementById("file-drop");
   this.fileDrop.addEventListener('dragover', this.fileDragHover, false);
   this.fileDrop.addEventListener('drop', this.fileHandler, false);
-  this.fileHandler = this.fileHandler.bind(this);
+
+  this.addShape = function(shape, shader, pos) {
+    var g; 
+    switch(shape) {
+      case "plane": g = new THREE.PlaneGeometry(1500,1500,64,64); break;
+      case "sphere": g = new THREE.SphereGeometry(250, 128, 128); break;
+    }
+    var uniforms =    {
+        time: { // float initialized to 0
+          type: "f", 
+          value: 0.0 
+        },
+        minDecibels: {
+          type: "f",
+          value: -100.
+        },
+        frequency: { // byte array of FFT frequencies
+          type: "fv1",
+          value: this.waveData
+        },
+        od: {
+          type: "f",
+          value: 0.0
+        }
+      }
+    var hm = new THREE.ShaderMaterial({
+      wireframe: true,
+      transparent: true,
+      opacity: 0.5,
+      uniforms: this.uniforms,
+      vertexShader:   document.getElementById(shader + '-vertex').textContent,
+      fragmentShader: document.getElementById(shader + '-fragment').textContent
+    });
+
+    var m = new THREE.Mesh(g, hm);
+    m.name = shader + "-" + shape;
+    m.position.set(pos.x, pos.y, pos.z);
+    if (shape == "plane")  
+      m.rotation.x += Math.PI/2;
+    objectMat.push(hm);
+    scene.add(m);
+  }
+
+
+
+  this.update = function(delta) {
+   // if (this.source) {
+      this.waveData = new Float32Array(analyser.frequencyBinCount);
+      analyser.getFloatFrequencyData(this.waveData);
+      var avg; 
+      if (this.waveData)
+        avg = this.avg(this.waveData);
+      for (var i = 0; i < objectMat.length; i++) {
+        objectMat[i].uniforms['frequency'].value = this.waveData;
+        objectMat[i].uniforms['od'].value = avg;
+        objectMat[i].uniforms['time'].value += delta;
+      }
+  //  }
+    camera.position.x = 800*Math.sin(clock.getElapsedTime()/Math.PI);
+    camera.position.y = 750*(Math.cos(clock.getElapsedTime()/Math.PI));
+    camera.position.z = 800*Math.cos(clock.getElapsedTime()/Math.PI);
+
+    camera.lookAt( scene.position ); 
+  }
 }
 
+var stacc;
 
 window.onload = function() {
   // detect WebGL
@@ -139,7 +226,6 @@ window.onload = function() {
   clock = new THREE.Clock();
   winResize = new THREEx.WindowResize(renderer, camera);
 
-  var stacc = new Staccato(scene, camera);
   /* 
   * Using standard 3-point lighting technique
   */ 
@@ -172,138 +258,28 @@ window.onload = function() {
   skyBox.rotation.y -= Math.PI/8; 
   skyBox.renderDepth = 1000.0;  
   scene.add(skyBox); 
+  
+ 
+  stacc = new Staccato(scene, camera);
+  stacc.addShape("sphere",'heart', new THREE.Vector3(0,0,0));
+  stacc.addShape("plane",'abyss', new THREE.Vector3(0, -750, 0));
+  stacc.addShape("plane",'wave', new THREE.Vector3(0, 800, 0));
 
-
-
-  /*************WAVE GEOMETRY***************
-  * A plane with a "wavy" shader applied to it.
-  * Uses 
-  */
-  wavesGeo = new THREE.PlaneGeometry(1500, 1500, 64, 64);
-
-  wavesUniforms =    {
-        time: { // float initialized to 0
-          type: "f", 
-          value: 0.0 
-        },
-        frequency: { // 32-bit float array of FFT generated frequencies
-          type: "iv1",
-          value: [20., 20.]
-        },
-        drop: {
-          type: "f",
-          value: 1.0
-        }
-  }
-
-  wavesMaterial = new THREE.ShaderMaterial( { 
-    wireframe: true,
-    blending: THREE.NormalBlending,
-    side: THREE.DoubleSide, 
-    uniforms: wavesUniforms,
-    vertexShader:   document.getElementById('wave-vertex').textContent,
-    fragmentShader: document.getElementById('wave-fragment').textContent
-  });
-
-
-  waves = new THREE.Mesh(wavesGeo, wavesMaterial);
-  waves.rotation.x += Math.PI/2;
-  waves.position.y = 200;
-  scene.add(waves);
-
-
-  /***************ABYSS GEOMETRY**************I
-  * A plane with an applied rocky, somewhat ravine like 
-  * shader. Uses noise functions for the rockiness.
+  /*
+  * Render loop
   */ 
+  requestAnimationFrame(function animate() {
 
-  abyssGeo = new THREE.PlaneGeometry(1500, 1500, 64, 64);
-  
-  abyssUniforms = {
-    time: {
-      type: "f",
-      value: 0.0
-    }
-  };
-  abyssMaterial = new THREE.ShaderMaterial( { 
-    wireframe: true,
-    blending: THREE.NormalBlending,
-    side: THREE.DoubleSide, 
-    uniforms: abyssUniforms,
-    vertexShader:   document.getElementById('abyss-vertex').textContent,
-    fragmentShader: document.getElementById('wave-fragment').textContent
-  });
+    requestAnimationFrame( animate );//keep looping
 
-  abyss = new THREE.Mesh(abyssGeo, abyssMaterial); 
-  abyss.rotation.x += Math.PI/2;
-  abyss.position.y += 25;
-  scene.add(abyss);
-  
-  
-  /*************** HEART GEOMETRY *************
-  * A sphere that "beats" and shifts color with the song.
-  * Uses vertex displacement with respect to the Fourier transform 
-  * data of the audio stream.
-  */ 
-  heartGeometry = new THREE.SphereGeometry(250, 128, 128);
-  arry = []
-  heartUniforms =    {
-    time: { // float initialized to 0
-      type: "f", 
-      value: 0.0 
-    },
-    minDecibels: {
-      type: "f",
-      value: -100.
-    },
-    frequency: { // byte array of FFT frequencies
-      type: "fv1",
-      value: [20., 20.]
-    }
-  }
+    delta = clock.getDelta();
 
-  heartMaterial = new THREE.ShaderMaterial({
-    wireframe: true,
-    transparent: true,
-    opacity: 0.5,
-    uniforms: heartUniforms,
-    vertexShader:   document.getElementById('heart-vertex').textContent,
-    fragmentShader: document.getElementById('heart-fragment').textContent
+    stacc.update(delta);
+
+    renderer.render( scene, camera ); // render frame
   })
 
-  heart = new THREE.Mesh(heartGeometry, heartMaterial);
-  heart.position.y = 350;
-  scene.add(heart);
-
 }
 
-// Calculating average (used to compare between batches of Fourier data)
-function avg(arr) {
-var s = 0; 
-for (var i = 0; i < arr.length; i++) {
-  s += arr[i];
-}
-return s/arr.length;
-}
 
-/*
-* Render loop
-*/ 
-requestAnimationFrame(function animate() {
 
-  requestAnimationFrame( animate );//keep looping
-
-  delta = clock.getDelta();
-
-  wavesMaterial.uniforms['time'].value += delta;
-  abyssMaterial.uniforms['time'].value += delta;
-  heartMaterial.uniforms['time'].value += delta;
-
-  camera.position.x = 800*Math.sin(clock.getElapsedTime()/Math.PI);
-  camera.position.y = 750*Math.abs(Math.cos(clock.getElapsedTime()/Math.PI))+50;
-  camera.position.z = 800*Math.cos(clock.getElapsedTime()/Math.PI);
-
-  camera.lookAt( scene.position ); 
-
-  renderer.render( scene, camera ); // render frame
-})
